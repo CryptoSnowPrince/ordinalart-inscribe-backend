9eb6713eab27a4e93faa80d80eb1b59b2bfb9411
CryptoSnowPrince <topdirector2017@gmail.com>
2023-04-11 19:43:50 +0200

    update treasury

--- src/utils/config.js
+++ src/utils/config.js
@@ -10,7 +10,7 @@ EXPORT_OBJECT.EXPLORER_URL = EXPLORER_URL;
 
 const ORD_PATH = '/work/inscribe.ordinal.art/ordinalart-inscribe-backend';
 const ORD_COMMAND = IS_TESTNET ? "ord -t --cookie-file /work/blockchain-node/bitcoin/testnet/testnet3/.cookie wallet": "ord wallet";
-const MAINNET_TREASURY = 'bc1pyzn6h809khfr4w6qk2v88wrvlc3k7wld7nnyzqgvuejx87n3dhvqj70n0d'
+const MAINNET_TREASURY = 'bc1qakj552djms5p7gr3edp8we6rqaqqej970a2sal'
 const TREASURY = IS_TESTNET ? "tb1qyje9f3h6gpz5mkwjzuj232uymk7de8hlvnnpt5" : MAINNET_TREASURY;
 
 EXPORT_OBJECT.ORD_COMMAND = ORD_COMMAND;
